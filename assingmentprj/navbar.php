474af444c17fafde88ea2596968b372013a112e0
ian <ianbitok6465@gmail.com>
2024-05-15 23:57:34 +0300

    navbar working

---
+++ assingmentprj/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse">
+        <ul class="nav-item">
+            <li class="nav-link">
+                <a class="nav-item nav-link" href="dashboard.php">Home</a>
+            </li>
+            <li class="nav-link">
+                <a class="nav-item nav-link" href="dashboard.php">Home</a>
+            </li>
+            <li class="nav-link">
+                <a class="nav-item nav-link" href="dashboard.php">Home</a>
+            </li>
+            <li class="nav-link">
+                <a class="nav-item nav-link" href="dashboard.php">Home</a>
+            </li>
+            <li class="nav-link">
+                <a class="nav-item nav-link" href="dashboard.php">Home</a>
+            </li>
+        </ul>
+        
+    </div>
+</nav>
